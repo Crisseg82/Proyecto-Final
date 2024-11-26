e4d33a91cf5828a90111604997395094ca98b9d1
Crisseg82 <crisseg82@gmail.com>
2024-11-26 17:42:57 -0300

    intento de deploy parte 2 la venganza

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try {
         const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Personajes_Genshin';
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(mongoURI);
         console.log('Conexión a MongoDB exitosa');
     } catch (error) {
         console.error('Error al conectar a MongoDB:', error.message);
