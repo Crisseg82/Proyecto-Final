253f19d008e608e5ff7d4e3ce5d70bfbfc256e11
Crisseg82 <crisseg82@gmail.com>
2024-11-27 16:12:51 -0300

    probando la info desde local porque el render me odia

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://proyecto-final-ejj7.onrender.com/api'
+    baseURL: 'http://localhost:5000/api'
 });
 
 export default axiosInstance;
